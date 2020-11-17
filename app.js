898864fbf6e2aa2b6aefa767c527bf8b85c74858
Paul Antoine Gast <pagast.pro@gmail.com>
2020-11-17 11:04:45 +0100

    Modiftication des délais des liens de laNavbar

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
  const navSlide = () => {
-     const burger = document.querySelector('.burger');
-     const nav    = document.querySelector('.nav-links');
-     const navLinks = document.querySelectorAll('.nav-links li');
+    const burger = document.querySelector('.burger');
+    const nav    = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-     burger.addEventListener('click', ()=>{
-         nav.classList.toggle('nav-active');
-     });
-     navLinks.forEach((link, index) => {
-         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`
-     });
- }
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+        });
+    });
+    }
 
 navSlide();
