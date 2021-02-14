c707dd10fb5db35b9e59054e21e9c06a5710ab67
Paul Antoine Gast <pagast.pro@gmail.com>
2021-02-14 09:37:06 +0100

    Suppression des commentaires

--- projets/apimeteo/script.js
+++ projets/apimeteo/script.js
@@ -7,20 +7,17 @@ if("geolocation" in navigator) {
         + position.coords.latitude + '&lon='
         + position.coords.longitude + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
     
-    let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-    requete.open('GET', url); // Nous récupérons juste des données
-    requete.responseType = 'json'; // Nous attendons du JSON
-    requete.send(); // Nous envoyons notre requête
+    let requete = new XMLHttpRequest(); 
+    requete.open('GET', url);
+    requete.responseType = 'json';
+    requete.send();
 
-    // Dès qu'on reçoit une réponse, cette fonction est executée
     requete.onload = function() {
       if (requete.readyState === XMLHttpRequest.DONE) {
         if (requete.status === 200) {
           let reponse = requete.response;
-          // console.log(reponse);
           let temperature = reponse.main.temp;
           let ville       = reponse.name;
-          // console.log(temperature);
           document.querySelector('#temperature_label').textContent = temperature;
           document.querySelector('#ville').textContent = ville;
         }
@@ -54,20 +51,18 @@ function erreur() {
 function recevoirTemperature(ville) {
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
 
-  let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
-  requete.open('GET', url); // Nous récupérons juste des données
-  requete.responseType = 'json'; // Nous attendons du JSON
-  requete.send(); // Nous envoyons notre requête
+  let requete = new XMLHttpRequest(); 
+  requete.open('GET', url);
+  requete.responseType = 'json';
+  requete.send();
 
-  // Dès qu'on reçoit une réponse, cette fonction est executée
   requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) {
       if (requete.status === 200) {
         let reponse = requete.response;
-        // console.log(reponse);
         let temperature = reponse.main.temp;
         let ville       = reponse.name;
-        // console.log(temperature);
+        
         document.querySelector('#temperature_label').textContent = temperature;
         document.querySelector('#ville').textContent = ville;
       }
